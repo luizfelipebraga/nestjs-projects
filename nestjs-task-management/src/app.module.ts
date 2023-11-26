391999070eceef309da3ed9fbfbbf417f8d9005f
luiz felipe <luizfelipebragaa@gmail.com>
2023-11-25 22:49:19 -0300

    feat: Connecting docker with bd

--- nestjs-task-management/src/app.module.ts
+++ nestjs-task-management/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task_db',
+      autoLoadEntities: true,
+      synchronize: true
+    })],
 })
-export class AppModule {}
+export class AppModule { }
