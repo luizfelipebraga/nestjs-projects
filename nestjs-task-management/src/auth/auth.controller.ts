1a2390ac63bdde5a6d75f503b377b9eef29722bf
luiz felipe <luizfelipebragaa@gmail.com>
2024-01-01 12:32:42 -0300

    feat: auth controller

--- nestjs-task-management/src/auth/auth.controller.ts
+++ nestjs-task-management/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {
+    this.authService = authService;
+  };
+
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+}
